76f7628822b0c40aac9ec62fc877ff26b32a0521
sanjay-TN <sanjaysanjaytn77@gmail.com>
2025-02-18 19:21:59 +0530

    Add initial files

---
+++ Operators.py
@@ -0,0 +1,37 @@
+#operators
+#assignment operator
+a=10
+a*=100 #a=a*100
+a+=100 #a=a+100
+
+print(a)
+
+#comparision operator
+#it includes(<,>,<=,>=,==,!=)
+a=10
+b=20
+print(a==b) #return true or false value
+print(a!=b) #compares a is not equal to b 
+
+#logical operators
+#it includes Logical(and,or,not)
+print(True and True) #returns tre if both conditions are true
+print(True and False)
+print(True or False) #returns true if one of the condition is true
+print(not(True)) #it changes true to false and vice-versa
+
+#membership operators
+#used to check an element is a membere of a string or any data
+a="sanjay"
+b="hellosanjay"
+print(("s" in a) and ("s" in b)) #returns true(because both a and b have s)
+
+#bitwise opearators
+#perform opearations on binary level on bits
+#it includes(& | !)
+a=10
+b=20
+print(a | b)
+
+
+
